dfbf7cd78f93e436c8115a2a8afb721f8147738f
unknown <2796125134@qq.com>
2020-12-05 19:12:50 +0800

    第一次提交登录功能

---
+++ wx/src/views/Login.vue
@@ -0,0 +1,28 @@
+<template>
+    <div> 
+            我是登录的页面
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
